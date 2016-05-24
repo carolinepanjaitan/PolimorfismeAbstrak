66fd9dbb72670aed9c6737bfb24b85edb371030c
carolinepanjaitan <carolinepanjaitan5@gmail.com>
2016-05-24 21:01:45 +0700

    Latihan

---
+++ XMain.java
@@ -0,0 +1,9 @@
+package com.polymorphism.abstrak;
+
+public class XMain {
+    public static void main(String[] args) {
+        Polymorps poly = new Polymorps();
+        poly.poly(new XLavender());
+        poly.poly(new XLily());
+}
+}
